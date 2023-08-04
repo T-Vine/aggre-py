f406680e7aecc3bf31359801183f3149ded80516
T-Vine <132595704+T-Vine@users.noreply.github.com>
2023-08-04 14:51:34 +0000

    Init constants, scraping

---
+++ aggre-py/constants.py
@@ -0,0 +1,11 @@
+"""
+Contains global constants for what is being parsed.
+"""
+
+# 
+INDEPENDENT = "https://www.independent.co.uk"
+BBC = "https://www.bbc.co.uk"
+
+# Class names.
+IND_TITLE = "title"
+BBC_TITLE = "ssrcss-its5xf-PromoLink"

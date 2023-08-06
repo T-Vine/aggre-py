6cd48e2d95731f8e6dcd80c4452ce22dc36cb352
T-Vine <132595704+T-Vine@users.noreply.github.com>
2023-08-06 18:08:41 +0100

    Titles Scraped

--- aggre-py/scraping.py
+++ aggre-py/scraping.py
@@ -1,3 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 import constants as c
+
+
+request = requests.get(c.INDEPENDENT)
+soup = BeautifulSoup(request.content,  "lxml-xml")
+titles = []
+for i in soup.find_all(attrs={"class": c.IND_TITLE}):
+    titles.append(i)

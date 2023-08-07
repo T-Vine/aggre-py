88cde832bc793bb5f9d4ce36c713adc586afe3df
T-Vine <132595704+T-Vine@users.noreply.github.com>
2023-08-07 07:52:43 +0100

    Update scraping.py
    Link Functionality

--- aggre-py/scraping.py
+++ aggre-py/scraping.py
@@ -6,5 +6,9 @@ import constants as c
 request = requests.get(c.INDEPENDENT)
 soup = BeautifulSoup(request.content,  "lxml-xml")
 titles = []
+links = []
 for i in soup.find_all(attrs={"class": c.IND_TITLE}):
     titles.append(i)
+    links.append(i.get("href"))
+print(len(titles))
+print(len(links))

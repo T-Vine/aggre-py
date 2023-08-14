555a22595489a053b89ab832666f17c92952fa29
T-Vine <132595704+T-Vine@users.noreply.github.com>
2023-08-14 20:20:48 +0100

    Removed Promo Codes, Added Link Texts

--- aggre-py/scraping.py
+++ aggre-py/scraping.py
@@ -2,13 +2,20 @@ from bs4 import BeautifulSoup
 import requests
 import constants as c
 
-
-request = requests.get(c.INDEPENDENT)
-soup = BeautifulSoup(request.content,  "lxml-xml")
+# Globals.
 titles = []
 links = []
+
+request = requests.get(c.INDEPENDENT)
+soup = BeautifulSoup(request.content,  "html.parser")
+
 for i in soup.find_all(attrs={"class": c.IND_TITLE}):
-    titles.append(i)
-    links.append(i.get("href"))
-print(len(titles))
-print(len(links))
+    if i.get("href") == None:
+        pass
+    else:
+        if "voucher" not in i.get("href"): # Ignoring if it is a promo link.
+            titles.append(i.get_text())
+            links.append(i.get("href"))
+
+print(titles)
+print(links)

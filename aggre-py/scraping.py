07e833b582bcbe95092b7db20f2033a000978578
T-Vine <132595704+T-Vine@users.noreply.github.com>
2023-08-14 20:51:50 +0100

    Update scraping.py
    With Time Functionality For Testing

--- aggre-py/scraping.py
+++ aggre-py/scraping.py
@@ -1,21 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
+import time
 import constants as c
 
 # Globals.
 titles = []
 links = []
 
+start_time = time.time()
+
 request = requests.get(c.INDEPENDENT)
 soup = BeautifulSoup(request.content,  "html.parser")
 
 for i in soup.find_all(attrs={"class": c.IND_TITLE}):
     if i.get("href") == None:
-        pass
+        pass    
     else:
         if "voucher" not in i.get("href"): # Ignoring if it is a promo link.
             titles.append(i.get_text())
             links.append(i.get("href"))
 
+end_time = time.time() - start_time
+
 print(titles)
 print(links)
+print(end_time)

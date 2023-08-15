0c5bf56da7f185c5372a51b88edadb2c387bb186
T-Vine <132595704+T-Vine@users.noreply.github.com>
2023-08-15 20:11:00 +0100

    Update scraping.py
    OOP Approach

--- aggre-py/scraping.py
+++ aggre-py/scraping.py
@@ -9,48 +9,51 @@ import asyncio
 import constants as c
 
 # Globals.
-indTitles: list[str] = []
-bbcTitles: list[str] = []
+class Scraping:
+    indTitles: list[str] = []
+    bbcTitles: list[str] = []
 
-indLinks: list[str] = []
-bbcLinks: list[str] = []
+    indLinks: list[str] = []
+    bbcLinks: list[str] = []
 
-start_time = time.time()
+    @staticmethod
+    async def toSoup(site: str) -> BeautifulSoup:
+        request = requests.get(site)
+        soup = BeautifulSoup(request.content,  "html.parser")
+        return soup 
 
-async def toSoup(site: str) -> BeautifulSoup:
-    request = requests.get(site)
-    soup = BeautifulSoup(request.content,  "html.parser")
-    return soup 
-
-async def parseMainData(site: str, title: str, titles: list[str], links: list[str]): 
-    text: str = ""
-    soup = await toSoup(site)
+    @classmethod
+    async def parseMainData(cls, site: str, title: str, titles: list[str], links: list[str]): 
+        text: str = ""
+        soup = await cls.toSoup(site)
     
-    for i in soup.find_all(attrs={"class": title}):
-        text = i.get_text()
-        if (i.get("href") == None):
-            pass    
-        else:
-            # Ignoring unwanted extras.
-            if (text == "Scotland") or (text == "ALBA") or (text == "Wales") or (text == "Cymru") or (text == "NI"):
-                    pass
-            elif ("voucher" not in i.get("href")): # Ignoring if it is a promo link.
-                if 'Audio' or 'Video' in text: # Ignoring audio/video.
-                        index = text.find("minutes") + 7
-                        text = text[index: ]
+        for i in soup.find_all(attrs={"class": title}):
+            text = i.get_text()
+            if (i.get("href") == None):
+                pass    
+            else:
+                # Ignoring unwanted extras.
+                if (text == "Scotland") or (text == "ALBA") or (text == "Wales") or (text == "Cymru") or (text == "NI"):
+                        pass
+                elif ("voucher" not in i.get("href")): # Ignoring if it is a promo link.
+                    if ("Audio" or "Video" in text) and ("minutes" in text) and (site != c.INDEPENDENT): # Ignoring audio/video.
+                            index = text.find("minutes") + 7
+                            text = text[index: ]
                 
-                titles.append(text)
-                links.append(i.get("href"))
-            
-async def main():
-    await asyncio.gather(parseMainData(c.INDEPENDENT, c.IND_TITLE, indTitles, indLinks), 
-                        parseMainData(c.BBC, c.BBC_TITLE, bbcTitles, bbcLinks))
-    print(indTitles)
-    print(bbcTitles)
+                    titles.append(text)
+                    links.append(i.get("href"))
+                    
+    @classmethod        
+    async def main(cls):
+        await asyncio.gather(cls.parseMainData(c.INDEPENDENT, c.IND_TITLE, cls.indTitles, cls.indLinks), 
+                            cls.parseMainData(c.BBC, c.BBC_TITLE, cls.bbcTitles, cls.bbcLinks))
 
 
 if (__name__ == "__main__"):
-    asyncio.run(main())
+    start_time = time.time()
+    asyncio.run(Scraping.main())
     end_time = time.time() - start_time
+    print(Scraping.indTitles)
     print(end_time)
+    
 
